cc06e4b2c60f8fce92573e13bda0c87af7824c6c
Michelle Parlevliet <michelle0parlevliet@gmail.com>
2023-03-21 10:20:55 -0400

    add programs and services classes and tables

---
+++ HospitalProject2/Migrations/202303211356267_programs.cs
@@ -0,0 +1,28 @@
+﻿namespace HospitalProject2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class programs : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Programs",
+                c => new
+                    {
+                        program_id = c.Int(nullable: false, identity: true),
+                        name = c.String(),
+                        department_id = c.Int(nullable: false),
+                        description = c.String(),
+                    })
+                .PrimaryKey(t => t.program_id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Programs");
+        }
+    }
+}
