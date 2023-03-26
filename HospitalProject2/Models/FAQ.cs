f8f21e2ef9d00f40205b61ae43ab0e4d8a6573ed
tamaraebi <tamaraebiukuli@gmail.com>
2023-03-26 16:39:55 -0400

    update

---
+++ HospitalProject2/Models/FAQ.cs
@@ -0,0 +1,28 @@
+﻿using HospitalProject2.Migrations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace HospitalProject2.Models
+{
+    public class FAQ
+    {
+        Key]
+        public int FAQ_Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        [ForeignKey("Department")]
+        public int department_Id { get; set; }
+
+    }
+
+    public class FAQDto
+    {
+        public int FAQ_Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public int department_Id { get; set; }
+    }
+}
