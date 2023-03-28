024ce73c69ab538274b3fa84f9b6e2e27b471e45
Michelle Parlevliet <michelle0parlevliet@gmail.com>
2023-03-28 10:31:06 -0400

    added programs and services data controllers

---
+++ HospitalProject2/Controllers/ProgramsDataController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using HospitalProject2.Models;
+
+namespace HospitalProject2.Controllers
+{
+    public class ProgramsDataController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/ProgramsData/ListPrograms
+        [HttpGet]
+        public IEnumerable<ProgramsDto> ListPrograms()
+        {
+            List<Programs> Programs = db.Programs.ToList();
+            List<ProgramsDto> ProgramsDtos = new List<ProgramsDto>();
+
+            Programs.ForEach(p => ProgramsDtos.Add(new ProgramsDto()
+            {
+                program_id = p.program_id,
+                name = p.name,
+                department_id = p.department_id,
+                description = p.description
+            }));
+
+            return ProgramsDtos;
+        }
+
+        // GET: api/ProgramsData/FindProgram/5
+        [ResponseType(typeof(Programs))]
+        [HttpGet]
+        public IHttpActionResult FindProgram(int id)
+        {
+            Programs Programs = db.Programs.Find(id);
+            ProgramsDto ProgramsDto = new ProgramsDto()
+            {
+                program_id = Programs.program_id,
+                name = Programs.name,
+                department_id = Programs.department_id,
+                description = Programs.description
+            };
+            if (Programs == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(ProgramsDto);
+        }
+
+        // UPDATE: api/ProgramsData/UpdateDepartment/5
+        [ResponseType(typeof(void))]
+        [HttpPost]
+        public IHttpActionResult UpdateProgram(int id, Programs programs)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != programs.program_id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(programs).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProgramsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // ADD: api/ProgramsData/AddProgram
+        [ResponseType(typeof(Programs))]
+        [HttpPost]
+        public IHttpActionResult AddProgram(Programs programs)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Programs.Add(programs);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = programs.program_id }, programs);
+        }
+
+        // DELETE: api/ProgramsData/DeleteProgram/5
+        [ResponseType(typeof(Programs))]
+        [HttpPost]
+        public IHttpActionResult DeleteProgram(int id)
+        {
+            Programs programs = db.Programs.Find(id);
+            if (programs == null)
+            {
+                return NotFound();
+            }
+
+            db.Programs.Remove(programs);
+            db.SaveChanges();
+
+            return Ok(programs);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProgramsExists(int id)
+        {
+            return db.Programs.Count(e => e.program_id == id) > 0;
+        }
+    }
+}
---
+++ HospitalProject2/Controllers/ServicesDataController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using HospitalProject2.Models;
+
+namespace HospitalProject2.Controllers
+{
+    public class ServicesDataController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/ServicesData/ListServices
+        [HttpGet]
+        public IEnumerable<ServicesDto> ListServices()
+        {
+            List<Services> Services = db.Services.ToList();
+            List<ServicesDto> ServicesDtos = new List<ServicesDto>();
+
+            Services.ForEach(s => ServicesDtos.Add(new ServicesDto()
+            {
+                service_id = s.service_id,
+                name = s.name,
+                program_id = s.program_id,
+                description = s.description,
+                location = s.location
+            }));
+
+            return ServicesDtos;
+        }
+
+        // GET: api/ServicesData/FindService/5
+        [ResponseType(typeof(Services))]
+        [HttpGet]
+        public IHttpActionResult FindService(int id)
+        {
+            Services services = db.Services.Find(id);
+            ServicesDto ServicesDto = new ServicesDto()
+            {
+                service_id = services.service_id,
+                name = services.name,
+                program_id = services.program_id,
+                description = services.description,
+                location = services.location
+            };
+            if (services == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(ServicesDto);
+        }
+
+        // UPDATE: api/ServicesData/UpdateService/5
+        [ResponseType(typeof(void))]
+        [HttpPost]
+        public IHttpActionResult UpdateService(int id, Services services)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != services.service_id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(services).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ServicesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // ADD: api/ServicesData/AddService
+        [ResponseType(typeof(Services))]
+        [HttpPost]
+        public IHttpActionResult AddServices(Services services)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Services.Add(services);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = services.service_id }, services);
+        }
+
+        // DELETE: api/ServicesData/DeleteService/5
+        [ResponseType(typeof(Services))]
+        [HttpPost]
+        public IHttpActionResult DeleteService(int id)
+        {
+            Services services = db.Services.Find(id);
+            if (services == null)
+            {
+                return NotFound();
+            }
+
+            db.Services.Remove(services);
+            db.SaveChanges();
+
+            return Ok(services);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ServicesExists(int id)
+        {
+            return db.Services.Count(e => e.service_id == id) > 0;
+        }
+    }
+}
