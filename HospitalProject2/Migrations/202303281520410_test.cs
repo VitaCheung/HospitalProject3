b13b7d1236b033acf0dd77010b3eb41e1daf5ec1
VitaCheung <ctw1207@gmail.com>
2023-03-28 09:02:58 -0700

    edit the test migration

--- HospitalProject2/Migrations/202303281520410_test.cs
+++ HospitalProject2/Migrations/202303281520410_test.cs
@@ -7,23 +7,13 @@
     {
         public override void Up()
         {
-            CreateTable(
-                "dbo.Donations",
-                c => new
-                {
-                    donations_id = c.Int(nullable: false, identity: true),
-                    department_id = c.Int(nullable: false),
-                    name = c.String(),
-                    email = c.String(),
-                    amount = c.Decimal(nullable: false),
-                })
-                .PrimaryKey(t => t.donations_id);
+            
 
         }
 
         public override void Down()
         {
-            DropTable("dbo.Donations");
+            
         }
     }
 }
