8e5422cb16ec7e54ed70c9ed1a21a8add18992c2
vita <vita@DESKTOP-SANLI87>
2023-03-30 11:19:20 -0700

    add CareersController and VolunteersController, and their views

---
+++ HospitalProject2/Models/ViewModels/UpdateCareers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HospitalProject2.Models.ViewModels
+{
+    public class UpdateCareers
+    {
+        public CareersDto SelectedCareer { get; set; }
+
+        public IEnumerable<DepartmentsDto> DepartmentOptions { get; set; }
+
+    }
+}
---
+++ HospitalProject2/Models/ViewModels/UpdateVolunteers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HospitalProject2.Models.ViewModels
+{
+    public class UpdateVolunteers
+    {
+        public VolunteersDto SelectedVolunteer { get; set; }
+
+        public IEnumerable<DepartmentsDto> DepartmentOptions { get; set; }
+
+    }
+}
