57a4e57c1f7a78c0d212c782f88c658b165b6958
Michelle Parlevliet <michelle0parlevliet@gmail.com>
2023-04-01 19:02:04 -0400

    added base views for programs

---
+++ HospitalProject2/Models/ViewModels/DetailsProgram.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HospitalProject2.Models.ViewModels
+{
+    public class DetailsProgram
+    {
+        public ProgramsDto SelectedProgram { get; set; }
+
+        public DepartmentsDto RelatedDepartment { get; set; }
+
+    }
+}
