0525e9486124739856ea11e79fe43d0091b45517
Carrie Ng <114375756+carrie823@users.noreply.github.com>
2023-04-13 22:42:45 -0400

    Update DetailsAppointment.cs

--- HospitalProject2/Models/ViewModels/DetailsAppointment.cs
+++ HospitalProject2/Models/ViewModels/DetailsAppointment.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +7,10 @@ namespace HospitalProject2.Models.ViewModels
 {
     public class DetailsAppointment
     {
+        public AppointmentsDto SelectedAppointment { get; set; }
+        
+        public StaffsDto RelatedStaffs { get; set; }
+
+        public PatientsDto RelatedPatients { get; set; }
     }
 }
