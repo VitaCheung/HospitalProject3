22d26b1b3e1eb511584ac6bd2e6805a7e4cc76d7
louiseabrena <abrenalouise@gmail.com>
2023-04-18 14:41:01 -0400

    update relationship between tables

---
+++ HospitalProject2/Models/ViewModels/DetailsStaff.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HospitalProject2.Models.ViewModels
+{
+    public class DetailsStaff
+    {
+        public Staffs SelectedStaff { get; set; }   
+    }
+}
